651863ebcf186a488f6f8d2f0ab95f172aee805f
yoCkawa0 <18nc047@ms.dendai.ac.jp>
2021-11-14 22:15:20 +0900

    Add main.py

---
+++ restfulapi_sanic/main.py
@@ -0,0 +1,12 @@
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic(__name__)
+
+
+@app.route('/', methods=['GET'])
+async def hello_world(request):
+    return json({'Hello': 'World!'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
